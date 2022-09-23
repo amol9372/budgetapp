cbb6ddf95ae798744bdfcf626f57a72ed2bab5cd
Amol Singh <amol1.singh@scotiabank.com>
2022-09-22 21:42:33 -0400

    added aws ci/cd

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -14,7 +14,7 @@ aws --version
 echo "Pulling image and starting container"
 
 #aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 319897880590.dkr.ecr.ap-south-1.amazonaws.com
-docker login -u AWS -p $(aws ecr get-login-password --region ap-south-1) 667631227859.dkr.ecr.us-east-1.amazonaws.com
+docker login -u AWS -p $(aws ecr get-login-password --region us-east-1) 667631227859.dkr.ecr.us-east-1.amazonaws.com
 docker pull 667631227859.dkr.ecr.us-east-1.amazonaws.com/budgetapp
 #docker tag 319897880590.dkr.ecr.ap-south-1.amazonaws.com/user-service user-service
 docker run -it --env-file /home/ec2-user/docker/env_files/budgetapp.env -p 8081:8081 -d 667631227859.dkr.ecr.us-east-1.amazonaws.com/budgetapp
